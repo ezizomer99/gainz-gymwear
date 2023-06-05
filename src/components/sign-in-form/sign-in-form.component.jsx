19d10740651032c3ce003ab6a10515a20e865479
Eziz Ømer <ezizo18@uia.no>
2023-06-05 12:33:19 +0200

    migrated everything from scss to styled comps

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -6,7 +6,10 @@ import {
     signInAuthUserWithEmailAndPassword,
 } from "../../utils/firebase/firebase.utils";
 
-import "./sign-in-form.styles.scss";
+import {
+    SignInFormContainer,
+    SignInFormButtonsContainer,
+} from "./sign-in-form.styles";
 
 const defaultFormFields = {
     email: "",
@@ -61,7 +64,7 @@ const SignInForm = () => {
     };
 
     return (
-        <div className="sign-up-container">
+        <SignInFormContainer>
         <h2>Already have an account?</h2>
         <span>Sign in with your email and password</span>
         <form onSubmit={handleSubmit}>
@@ -81,7 +84,7 @@ const SignInForm = () => {
                 name="password"
                 value={password}
             />
-            <div className="buttons-container">
+            <SignInFormButtonsContainer>
                 <Button type="submit">Sign In</Button>
                 <Button 
                     type="button" 
@@ -90,10 +93,10 @@ const SignInForm = () => {
                     >
                     Google sign In
                 </Button>
-            </div>
+            </SignInFormButtonsContainer>
             
         </form>
-        </div>
+        </SignInFormContainer>
     );
 };
 
